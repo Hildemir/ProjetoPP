a13bf5833faca4ad34ed0a64ba195c14098b8dac
Bhilder <hildemir.regis@hotmail.com>
2019-04-17 20:32:26 -0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ClientModule clientModule = new ClientModule();
+        FlightModule flightModule = new FlightModule();
+        TicketModule ticketModule = new TicketModule();
+        int option;
+        do{
+            System.out.println("Menu Principal");
+            System.out.println("Escolha uma opcao:\n1-Clientes\n2-Voos\n3-Passagens\n4-Salvar e sair");
+            option = in.nextInt();
+            switch (option){
+                case 1:
+                    clientMenu(clientModule, in);
+                    break;
+                case 2:
+                    flightMenu(flightModule, in);
+                    break;
+                case 3:
+            }
+        }while(option!= 4);
+        Flight f;
+    }
     private static void clientMenu(ClientModule clientModule, Scanner in) {
         int option;
         do {
@@ -54,48 +76,27 @@ public class Main {
     }
 
     private static void ticketMenu(TicketModule ticketModule, Scanner in) {
-//        int option;
-//        do {
-//            System.out.println("Escolha uma opcao:\n1-Vender\n2-Consultar passagem\n3-Exibir\n4-Remover");
-//            option = in.nextInt();
-//            in.nextLine();
-//        } while(option > 4 || option < 1);
-//        switch (option) {
-//            case 1:
-//                flightModule.addFlight();
-//                break;
-//            case 2:
-//                flightModule.changeFlightPrice();
-//                break;
-//            case 3:
-//                flightModule.findFlight();
-//                break;
-//            case 4:
-//                flightModule.removeFlight();
-//                break;
-//        }
+        do {
+            System.out.println("Escolha uma opcao:\n1-Vender\n2-Consultar passagem\n3-Exibir\n4-Remover");
+        int option;
+            option = in.nextInt();
+            in.nextLine();
+        } while(option > 4 || option < 1);
+        switch (option) {
+            case 1:
+                flightModule.addFlight();
+                break;
+            case 2:
+                flightModule.changeFlightPrice();
+                break;
+            case 3:
+                flightModule.findFlight();
+                break;
+            case 4:
+                flightModule.removeFlight();
+                break;
+        }
     }
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        ClientModule clientModule = new ClientModule();
-        FlightModule flightModule = new FlightModule();
-        TicketModule ticketModule = new TicketModule();
-        int option;
-        do{
-            System.out.println("Menu Principal");
-            System.out.println("Escolha uma opcao:\n1-Clientes\n2-Voos\n3-Passagens\n4-Salvar e sair");
-            option = in.nextInt();
-            switch (option){
-                case 1:
-                    clientMenu(clientModule, in);
-                    break;
-                case 2:
-                    flightMenu(flightModule, in);
-                    break;
-                case 3:
-            }
-        }while(option!= 4);
-        Flight f;
-    }
+
 }
